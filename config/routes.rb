6d622ee03e12eaeaf017a75a3c506cfa8bf18c87
fstraube <franz.straube@sixt.com>
2021-04-15 21:02:35 +0200

    added vuetify

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
-root to: 'welcome#index'
-resources :users
+Rails.application.routes.draw do
+  root to: 'welcome#index'
+  resources :users
+  get 'app', to: 'users#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
