e7293b2d41d5d2746876c8168b653f7a7ee85f0f
fstraube <franz.straube@sixt.com>
2021-04-15 20:32:54 +0200

    generated controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,2 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+root to: 'welcome#index'
+resources :users
